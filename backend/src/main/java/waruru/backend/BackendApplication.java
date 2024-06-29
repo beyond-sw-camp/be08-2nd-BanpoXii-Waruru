e823bebd7d3d28c1e910505e01397cc9ba69c60b
limjihoon99 <bo9701@naver.com>
2024-06-29 22:44:55 +0900

    chore: springboot setting

--- backend/src/main/java/waruru/backend/BackendApplication.java
+++ backend/src/main/java/waruru/backend/BackendApplication.java
@@ -1,7 +1,13 @@
 package waruru.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BackendApplication {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(BackendApplication.class, args);
     }
+
 }
