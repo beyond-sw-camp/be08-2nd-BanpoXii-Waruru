8c558b19fd0e9ca9567e4fab12ac5bae9b2ce404
limjihoon99 <bo9701@naver.com>
2024-07-02 18:57:41 +0900

    feat: detail domain first commit

--- backend/src/main/java/waruru/backend/detail/domain/DetailEntity.java
+++ backend/src/main/java/waruru/backend/detail/domain/DetailEntity.java
@@ -1,4 +1,16 @@
 package waruru.backend.detail.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class DetailEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
