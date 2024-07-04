81e0379d62f66a21ae358bda15b2982859269df5
limjihoon99 <bo9701@naver.com>
2024-07-04 23:44:46 +0900

    feat: create DetailEntity

--- backend/src/main/java/waruru/backend/detail/domain/DetailEntity.java
+++ backend/src/main/java/waruru/backend/detail/domain/DetailEntity.java
@@ -1,16 +1,66 @@
 package waruru.backend.detail.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.catalina.User;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
 @Entity
+@Getter
+@Setter
 public class DetailEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @ManyToOne
+    @JoinColumn(name = "sale_no", nullable = false)
+    @JsonManagedReference
+    private Sale saleNo;
+
+    @ManyToOne
+    @JoinColumn(name = "user_no", nullable = false)
+    @JsonManagedReference
+    private User userNo;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String price;
+
+    @Column(nullable = false)
+    private String detailDate;
+
+    @CreationTimestamp
+    private LocalDateTime registDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
+
+    @Builder
+    public DetailEntity(Sale saleNo, User userNo, String title, String category, String description, String price, String detailDate) {
+        this.saleNo = saleNo;
+        this.userNo = userNo;
+        this.title = title;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+        this.detailDate = detailDate;
+    }
 }
