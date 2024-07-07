9850413c042a24d39d2e69583782a0268e2f065a
uzz99 <yj.lee991228@gmail.com>
2024-07-07 20:37:24 +0900

    feat: add review 등록과 조회를 위한 DTO #22

--- backend/src/main/java/waruru/backend/review/dto/ReviewResponseDTO.java
+++ backend/src/main/java/waruru/backend/review/dto/ReviewResponseDTO.java
@@ -1,4 +1,35 @@
 package waruru.backend.review.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import waruru.backend.review.domain.Review;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class ReviewResponseDTO {
+
+    private Long reviewNo;
+
+    private Long userId;
+
+    private Long saleNo;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime registerDate;
+
+    private LocalDateTime updateDate;
+
+    public ReviewResponseDTO(Review review) {
+        this.userId = review.getUserNo().getId();
+        this.saleNo = review.getSaleNo().getNo();
+        this.title = review.getTitle();
+        this.content = review.getContent();
+        this.registerDate = review.getRegisterDate();
+    }
+
 }
