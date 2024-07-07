70b0ed558924401c21f40a8345ea3684ed21ae53
uzz99 <yj.lee991228@gmail.com>
2024-07-07 20:43:57 +0900

    Revert "feat: add review 관리 및 등록 및 조회를 위한 API endpoints 설정 #22"
    This reverts commit 498fe29f6593216bdc3738e18cc1f88ce75b1a83.

--- backend/src/main/java/waruru/backend/review/controller/ReviewController.java
+++ backend/src/main/java/waruru/backend/review/controller/ReviewController.java
@@ -1,38 +1,4 @@
 package waruru.backend.review.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import waruru.backend.review.domain.Review;
-import waruru.backend.review.dto.ReviewRequestDTO;
-import waruru.backend.review.dto.ReviewResponseDTO;
-import waruru.backend.review.service.ReviewService;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/reviews")
-@Tag(name = "Review 관리", description = "리뷰 관리를 위한 api")
 public class ReviewController {
-    @Autowired
-    private ReviewService reviewService;
-
-    @PostMapping
-    @Operation(summary = "새 리뷰 등록", description = "[설명] /api/reviews 새 리뷰 등록")
-    public ResponseEntity<ReviewResponseDTO> createReview(@RequestBody ReviewRequestDTO reviewRequestDTO) {
-        ReviewResponseDTO response = reviewService.createReview(reviewRequestDTO);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/list")
-    @Operation(summary = "모든 리뷰 조회하기", description = "[설명] /api/reviews/list 모든 리뷰 조회하기")
-    public ResponseEntity<List<ReviewResponseDTO>> getAllReviews() {
-        List<ReviewResponseDTO> reviews = reviewService.getAllReviews();
-        return ResponseEntity.ok(reviews);
-    }
-
-
 }
