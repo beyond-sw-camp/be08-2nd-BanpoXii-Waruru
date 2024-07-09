33185722fa4ad34c754f2e0cbf99e3a893f25dd6
uzz99 <yj.lee991228@gmail.com>
2024-07-10 02:25:18 +0900

    feat: add review 수정 및 API endpoints #39

--- backend/src/main/java/waruru/backend/review/service/ReviewService.java
+++ backend/src/main/java/waruru/backend/review/service/ReviewService.java
@@ -3,10 +3,12 @@ package waruru.backend.review.service;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import waruru.backend.review.domain.Review;
 import waruru.backend.review.domain.ReviewRepository;
 import waruru.backend.review.dto.ReviewRequestDTO;
 import waruru.backend.review.dto.ReviewResponseDTO;
+import waruru.backend.review.dto.ReviewUpdateRequestDTO;
 import waruru.backend.sale.domain.Sale;
 import waruru.backend.sale.domain.SaleRepository;
 import waruru.backend.user.domain.User;
@@ -22,31 +24,31 @@ public class ReviewService {
     // 비즈니스 로직 수행을 위한
     private final ReviewRepository reviewRepository;
     private final UserRepository userRepository;
-//    private final SaleRepository salesRepository;
+    private final SaleRepository saleRepository;
 
     @Autowired
-    public ReviewService(ReviewRepository reviewRepository, UserRepository userRepository/*, SaleRepository salesRepository*/){
+    public ReviewService(ReviewRepository reviewRepository, UserRepository userRepository, SaleRepository salesRepository){
         this.reviewRepository = reviewRepository;
         this.userRepository = userRepository;
-//        this.salesRepository = salesRepository;
+        this.saleRepository = salesRepository;
     }
 
+    @Transactional
     public ReviewResponseDTO createReview(ReviewRequestDTO rrq){
         Review review = new Review();
 
         //  예외처리 해주기
         User user = userRepository.findById(rrq.getUserNo())
                 .orElseThrow(() -> new EntityNotFoundException("회원 ID 조회 불가 : " + rrq.getUserNo()));
-//        Sale sale = salesRepository.findById(rrq.getSaleNo())
-//                .orElseThrow(() -> new EntityNotFoundException("매물 번호 조회 불가 : " + rrq.getSaleNo()));
+        Sale sale = saleRepository.findById(rrq.getSaleNo())
+                .orElseThrow(() -> new EntityNotFoundException("매물 번호 조회 불가 : " + rrq.getSaleNo()));
 
 
         review.setUserNo(user);
-//        review.setSaleNo(sale);
+        review.setSaleNo(sale);
         review.setTitle(rrq.getTitle());
         review.setContent(rrq.getContent());
         review.setRegisterDate(rrq.getRegisterDate());
-
 
         review = reviewRepository.save(review);
 
@@ -57,6 +59,20 @@ public class ReviewService {
         return reviewRepository.findAll().stream()
                 .map(ReviewResponseDTO::new)
                 .collect(Collectors.toList());
+    }
+
+    //매물 후기 수정
+    public ReviewResponseDTO updateReview(Long reviewNo, ReviewUpdateRequestDTO reviewUpdateRequestDTO) {
+        Review review = reviewRepository.findById(reviewNo)
+                .orElseThrow(() -> new EntityNotFoundException("매물 후기 조회 불가: " + reviewNo));
+
+        review.setTitle(reviewUpdateRequestDTO.getTitle());
+        review.setContent(reviewUpdateRequestDTO.getContent());
+        review.setUpdateDate(reviewUpdateRequestDTO.getUpdateDate());
+
+        Review updatedReview = reviewRepository.save(review);
+
+        return new ReviewResponseDTO(updatedReview);
     }
 
 
