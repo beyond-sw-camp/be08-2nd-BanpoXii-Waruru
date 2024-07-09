7be45fd7aa3071b601fe3194435f1d340e73aed4
uzz99 <yj.lee991228@gmail.com>
2024-07-10 02:38:57 +0900

    feat: add review 수정 및 API endpoints #39

---
+++ backend/src/main/java/waruru/backend/review/dto/ReviewUpdateRequestDTO.java
@@ -0,0 +1,19 @@
+package waruru.backend.review.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ReviewUpdateRequestDTO {
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime updateDate;
+
+}
