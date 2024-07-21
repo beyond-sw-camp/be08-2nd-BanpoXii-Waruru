2a9e4dfea9354670ee54db369fd2915a10439fb4
henhen7 <eatho@naver.com>
2024-07-21 18:00:57 +0900

    refactor: sale 코드 리팩토링

--- backend/src/main/java/waruru/backend/sale/controller/SaleController.java
+++ backend/src/main/java/waruru/backend/sale/controller/SaleController.java
@@ -21,35 +21,35 @@ public class SaleController {
     @Autowired
     private SaleService saleService;
 
-    @PostMapping("/register") //OK
+    @PostMapping("/register")
     @Operation(summary = "매물 등록", description = "/api/sales 새 매물 등록")
     public ResponseEntity<SaleResponseDTO> registerSale(@RequestBody @Valid SaleRegisterRequestDTO registerRequestDTO) {
         SaleResponseDTO saleresponseDTO = saleService.registerSale(registerRequestDTO);
         return ResponseEntity.ok(saleresponseDTO);
     }
 
-    @GetMapping("/list") //OK
+    @GetMapping("/list")
     @Operation(summary = "모든 매물 리스트 조회", description = "/api/sales/list 매물 정보 리스트 조회(특정 필드)")
     public ResponseEntity<List<SaleListResponseDTO>> findAllList() {
         List<SaleListResponseDTO> lists = saleService.findAllList();
         return ResponseEntity.ok(lists);
     }
 
-    @GetMapping("/detail/{no}") // OK
+    @GetMapping("/detail/{no}")
     @Operation(summary = "상세 매물 조회", description = "/api/sales/detail/{no} 특정 매물 상세 조회(전체 필드)")
-    public ResponseEntity<SaleResponseDTO> findById(@PathVariable Long no) {
+    public ResponseEntity<SaleResponseDTO> findById(@PathVariable("no") Long no) {
         SaleResponseDTO saleResponseDTO = saleService.findById(no);
         return ResponseEntity.ok(saleResponseDTO);
     }
 
-    @DeleteMapping("/delete/{no}") // OK
+    @DeleteMapping("/delete/{no}")
     @Operation(summary = "매물 삭제", description = "/api/sales/delete/{no} 매물 삭제")
-    public ResponseEntity<Void> deleteSale(@PathVariable Long no) {
+    public ResponseEntity<Void> deleteSale(@PathVariable("no") Long no) {
         saleService.deleteSale(no);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PutMapping("/update/{no}") // OK
+    @PutMapping("/update/{no}")
     @Operation(summary = "매물 정보 수정", description = "/api/sales/update/{no} 매물 정보 수정")
     public ResponseEntity<Void> updateSale(@PathVariable("no") Long no,
                                            @RequestBody @Valid SaleUpdateRequestDTO updateRequestDTO) {
