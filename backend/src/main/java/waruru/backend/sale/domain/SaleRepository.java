7cd489fedff9ca8ad7acd118de1179d046162d32
henhen7 <eatho@naver.com>
2024-07-22 21:26:26 +0900

    refactor: sale 리뷰 수 카운트 기능 구현

--- backend/src/main/java/waruru/backend/sale/domain/SaleRepository.java
+++ backend/src/main/java/waruru/backend/sale/domain/SaleRepository.java
@@ -1,7 +1,10 @@
 package waruru.backend.sale.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+    @Query("select count(b) from Review b where b.saleNo.no = :sale_no")
+    Integer getReviewCount(@Param("sale_no") Long saleNo);
 }
