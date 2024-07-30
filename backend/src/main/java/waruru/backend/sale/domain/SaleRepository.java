3722b3bd3a70dfa9decbd9598df7d8726d6c7637
henhen7 <eatho@naver.com>
2024-07-30 10:15:51 +0900

    feat: Sale 페이징 처리 #96

--- backend/src/main/java/waruru/backend/sale/domain/SaleRepository.java
+++ backend/src/main/java/waruru/backend/sale/domain/SaleRepository.java
@@ -1,5 +1,7 @@
 package waruru.backend.sale.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,4 +10,7 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     @Query("select count(b) from Review b where b.saleNo.saleNo = :sale_no")
     Integer getReviewCount(@Param("sale_no") Long saleNo);
+
+    Page<Sale> findAll (Pageable pageable);
+
 }
